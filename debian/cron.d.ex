aefa6801bfe1b0469acf527f21487679d291a414
Elias Woods <EliasWoods@gmail.com>
2010-06-04 17:41:20 -0400

    deleted:    debian/README.Debian 	deleted:    debian/README.source 	deleted:    debian/changelog 	deleted:    debian/compat 	deleted:    debian/control 	deleted:    debian/copyright 	deleted:    debian/cron.d.ex 	deleted:    debian/docs 	deleted:    debian/emacsen-install.ex 	deleted:    debian/emacsen-remove.ex 	deleted:    debian/emacsen-startup.ex 	deleted:    debian/groove.default.ex 	deleted:    debian/groove.doc-base.EX 	deleted:    debian/init.d.ex 	deleted:    debian/init.d.lsb.ex 	deleted:    debian/manpage.1.ex 	deleted:    debian/manpage.sgml.ex 	deleted:    debian/manpage.xml.ex 	deleted:    debian/menu.ex 	deleted:    debian/postinst.ex 	deleted:    debian/postrm.ex 	deleted:    debian/preinst.ex 	deleted:    debian/prerm.ex 	deleted:    debian/rules 	deleted:    debian/watch.ex

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the groove package
-#
-0 4	* * *	root	[ -x /usr/bin/groove_maintenance ] && /usr/bin/groove_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(groove):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="groove" command="/usr/bin/groove"
